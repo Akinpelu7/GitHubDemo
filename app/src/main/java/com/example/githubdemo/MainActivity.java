d65d7600ed6b7c01cf81a0deb04aada44426eaf2
Akinpelu7 <omotosoe@student.douglascollege.ca>
2023-02-02 11:24:43 -0800

    second commit kkk

--- app/src/main/java/com/example/githubdemo/MainActivity.java
+++ app/src/main/java/com/example/githubdemo/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //comment
+
+    //local changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
